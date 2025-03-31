522159e5e9fd7397d1cddbc38aaa2dd8e59f33b3
Sasha <sashaart.97@gmail.com>
2025-03-31 20:19:28 +0300

    Added shadcn and first pages

---
+++ app/register/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+import { Routes } from "@/constants/routes";
+
+export default function Register() {
+  return <Link href={Routes.CONTENT}>Content</Link>;
+}
---
+++ constants/routes.ts
@@ -0,0 +1,6 @@
+export enum Routes {
+    HOME = "/",
+    LOGIN = "/login",
+    REGISTER = "/register",
+    CONTENT = "/content",
+  }
